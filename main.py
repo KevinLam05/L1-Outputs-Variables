e0af9f4421c02acae5a4ebb0b18fd957e5963bec
Kevin <107879311+KevinLam05@users.noreply.github.com>
2022-06-20 18:16:59 +0000

    Concatenation + Type cast

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 print("Hello World") # This prints out Hello World 
 print("My name is Kevin")
 print("Hello" + " World")
+myName = "Kevin" # String 
+myBirthMonth = 6 # Integer 
+myBirthDate = "18" # String 
+pi = 3.14 # float
+isHungry = True # Boolean
+
+print("My name is " + myName)
+print("My birth month is " + str(myBirthMonth))
